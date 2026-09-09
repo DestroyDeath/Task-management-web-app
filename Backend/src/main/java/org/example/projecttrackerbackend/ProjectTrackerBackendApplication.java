a1c9f4a790ec557e848310e1e6b2d276887ab31b
Aman Singh Bisht <bishtaman767@gmail.com>
2026-09-09 13:46:54 +0530

    Fix controller component scanning

--- Backend/src/main/java/org/example/projecttrackerbackend/ProjectTrackerBackendApplication.java
+++ Backend/src/main/java/org/example/projecttrackerbackend/ProjectTrackerBackendApplication.java
@@ -2,12 +2,13 @@ package org.example.projecttrackerbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.example.projecttrackerbackend")
 public class ProjectTrackerBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectTrackerBackendApplication.class, args);
     }
-
 }
